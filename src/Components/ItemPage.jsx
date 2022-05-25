fc21f6d4011dbec3fa919d51a838893a73eb4314
Aguda <agudaoluwanifemi@gmail.com>
2022-05-25 23:39:33 +0100

    fifth commit

--- src/Components/ItemPage.jsx
+++ src/Components/ItemPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ItemPage.css'
+import { BsFillHeartFill  } from "react-icons/bs";
 import left from '../Assets/leftarrow.svg'
 import heart from '../Assets/Heart.svg'
 import star from '../Assets/star.svg'
@@ -14,11 +15,32 @@ import Vector from '../Assets/Vector.svg'
 
 
 const ItemPage = () => {
+    const [toggle, setToggle] = useState(false)
+    const [counter, setCounter] = useState(1)
+
+    const love = () =>{
+       setToggle((toggle) => !toggle) 
+    }
+
+    const increment = () =>{
+        setCounter((prevCounter) => prevCounter + 1) 
+     }
+
+     const decrement = () =>{
+         if(counter < 1){
+             setCounter(0) 
+         }else{
+            setCounter((prevCounter) => prevCounter - 1) 
+         }
+     }
+
   return (
     <div className='bod'>
         <div className="navigation2">
             <img src={left} alt="" />
-            <img src={heart} alt="" />
+            <button onDoubleClick={love} className={toggle ? 'heart-button-enabled' : 'heart-button-disabled' }><BsFillHeartFill /> 
+            
+            </button>
         </div>
 
         <div className="prod2">
@@ -37,7 +59,7 @@ const ItemPage = () => {
         <div className="filter">
             <div className="size">
                 <p>size</p>
-                <p style={{margin: '0', border: '1px solid black', borderRadius: '50%', width: '24px', height: '24px', lineHeight: '24px'}}>9</p>
+                <p style={{margin: '0', border: '1px solid black', textAlign: 'center', borderRadius: '50%', width: '24px', height: '24px', lineHeight: '24px'}}>9</p>
             </div>
             <div className="size">
                 <p>color</p>
@@ -52,10 +74,10 @@ const ItemPage = () => {
             </div>
         </div>
 
-        <div style={{marginTop: '-150px'}}>
-            <img  src={bigshoe} alt="" />
+        <div  style={{marginTop: '-150px', display: 'flex', justifyContent: 'center'}}>
+            <img className='bigshoe' src={bigshoe} alt="" />
         </div>
-        <div style={{marginTop: '-120px'}}>
+        <div style={{marginTop: '-120px', display: 'flex', justifyContent: 'center'}}>
             <img   src={camera} alt="" />
         </div>
 
@@ -79,9 +101,9 @@ const ItemPage = () => {
 
             <div className='buttons'>
                 <div className="meter">
-                    <img src={sub} alt="" />
-                    <p>1</p>
-                    <img src={Vector} alt="" />
+                    <img onClick={decrement} src={sub} alt="" />
+                    <p>{counter}</p>
+                    <img onClick={increment} src={Vector} alt="" />
                 </div>
 
                 <div className="add-to-cart">

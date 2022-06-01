87ad4cead0dffd5a4ea4ac5cf3b806b214c6d0f9
Aguda <agudaoluwanifemi@gmail.com>
2022-06-01 23:21:41 +0100

    sixth commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import HomePage from './Components/HomePage';
 import ItemPage from './Components/ItemPage';
 
 function App() {
   
   return (
     <div className="App">
-      {/* <HomePage/> */}
       <ItemPage />
     </div>
   );
